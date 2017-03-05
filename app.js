3b988cf1f4c069464d397ba09f6bce87b052309b
Marusa Barle <marusa.barle@proteusnet.com>
2017-03-05 15:50:28 +0000

    Improved checkitem function.

--- app.js
+++ app.js
@@ -12,30 +12,50 @@ var addItem = function(state, item) {
     });
 
 }
+
+function getItem(state, itemIndex) {
+    console.log(itemIndex);
+    return state.items[itemIndex];
+    
+}
+
 // delete item
 var deleteItem = function(state, item) {
 	state.items.splice(item, 1);
 }
+
 // check item
 var checkItem = function(state, item) {
+
+
     
-    state.items[state.items.findIndex(x => x.displayName===item)].checkedOff = true;
+    var itemId = $(item.closest('li')).attr('data-list-item-id'); //itemId - dobiš ta index, ki je spodaj
+    var currentItem = getItem(state, itemId);
+ 
+    currentItem.checkedOff = true;
  
 };
 
-/* un-check item
+
+//var currentItem = state.items[$(item.closest('li')).attr('data-list-item-id')];
+
+// un-check item
 var unCheckItem = function(state, item) {
-    item.checkedOff = false;
-}*/
+    var itemId = $(item.closest('li')).attr('data-list-item-id');
+    var currentItem = getItem(state, itemId);
+  //  console.log(itemID);
+    currentItem.checkedOff = false; 
+}
 
 // Render functions
 var renderList = function(state, element) {
-    var itemsHTML = state.items.map(function(item) {
+    var itemsHTML = state.items.map(function(item, index) {
         if (item.checkedOff === false) {
-            return '<li><span class="shopping-item">' + item.displayName + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
+            return '<li data-list-item-id="'+ index +'"><span class="shopping-item">'
+             + item.displayName + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
         }
         else {
-            return '<li><span class="shopping-item shopping-item__checked">' + item.displayName + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
+            return '<li data-list-item-id="'+ index +'"><span class="shopping-item shopping-item__checked">' + item.displayName + '</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button> <button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>';
         }
     });
     element.html(itemsHTML);
@@ -71,22 +91,25 @@ $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
     console.log(state);
     console.log("Hello!")
     checkItem(state, this);
-   
+    
     renderList(state, $('.shopping-list'));
   //  $('.shopping-item').toggleClass("shopping-item__checked");
 });
 
-
-
-
-
-
-/*
-Questions:
-1. Is var addItem a function?
-2. preveri, če se splo shrani kaj v state, morda
-3. poglej closest();
-4. is it always neccessary to have (item) in
-
-
+/*$('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+    event.preventDefault();
+    console.log(state);
+    console.log("Hello!")
+    
+    unCheckItem(state, this);
+    
+    renderList(state, $('.shopping-list'));
+  //  $('.shopping-item').toggleClass("shopping-item__checked");
+});
 */
+
+
+
+
+
+
